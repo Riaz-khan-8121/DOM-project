03f1850f0d5e3ba7ef310c1ae07f394e45f94f6d
Riaz-khan-8121 <riazkakar8121940@gmail.com>
2025-09-16 23:59:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,121 @@
+// 1. Select a paragraph with id "message" and change its text to "Welcome!".
+
+// let para=document.querySelector("#para")
+
+// para.innerHTML = "Welcome!"
+
+
+
+// 2. Get the value from an input with id "nameInput" and display it inside a div with id
+// "result".
+
+
+// let input=document.querySelector("#nameInput")
+// let div=document.querySelector("#div")
+
+// function insideDiv() {
+
+//     div.innerHTML=input.value;
+//     input.value=""
+//     // div.innerHTML =""
+//     console.log(input)
+// }
+
+
+
+
+
+// 3. When a button is clicked, change the background color of a section with id
+// "container" to light blue.
+
+// let changecolor=document.querySelector("#changeColor")
+
+// function changeColor () {
+//     if(document.body.style.backgroundColor="red") {
+//         document.body.style.backgroundColor="red"
+//     } else {
+//         document.body.style.backgroundColor="blue"
+//     }
+// }
+
+
+// 4. Select a heading (h1) with id "mainTitle" and change its color to red.
+
+// let main=document.querySelector("#mainTitle")
+
+// function change() {
+//     main.style.color="blue"
+// }
+
+
+
+// 6. Select a button with class "submit-btn" and log "Button Clicked" to the console
+// when it's clicked.
+
+
+// let btn=document.querySelector("#submit-btn")
+// function click() {
+//     console.log("btn was clicked")
+// }
+
+
+// 7. Get values from two inputs with ids "first" and "second", add them (as numbers),
+// and show the result in a span with id "sum".
+
+
+
+
+// 8. Change the text of a paragraph with class "description" to "Updated
+// successfully".
+
+
+
+// 9. Select a div with id "box" and set its width to 200px and background color to green.
+
+// let div=document.querySelector("#div")
+// div.style.width="200px"
+// div.style.backgroundColor="blue"
+
+
+
+//  Get the email entered in an input with id "email" and print it in the console.
+
+
+// let email=document.querySelector("#email")
+
+// function change() {
+//     console.log(email.value)
+//     email.value=""
+// }
+
+
+
+
+// Select a paragraph and update its font size to 24px.
+
+// let para=document.querySelector("#para");
+// para.style.fontSize="25px"
+
+
+
+// Get a user’s age from an input with id "age" and display "Eligible" if age >= 18,
+// else "Not Eligible" in a span.
+
+
+// let age=document.querySelector("#age")
+// function check() {
+//     if(age.value > 18) {
+//         console.log("Eligible")
+//     } else {
+//         console.log("Not Eligible")
+//     }
+// }
+
+
+
+// When a button is clicked, clear the value of an input with id "searchBox".
+
+
+// function clear() {
+//     input.value="";
+// }
